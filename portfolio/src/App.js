0fdbca5ccf5a947592be703a860fec0ff48832c8
Tom_Court <tomcourt94@icloud.com>
2023-07-21 17:00:31 +0100

    homepage and about page content added, included animation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,7 +2,46 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-scroll";
+import { motion, useAnimation } from "framer-motion";
+import { useEffect, useState } from "react";
 import "./App.css";
+
+const ScalingDiv = ({ children, className }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const element = document.querySelector(className);
+      if (element) {
+        const elementTop = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        setIsVisible(elementTop < windowHeight);
+      }
+    };
+
+    const handleScale = () => {
+      if (isVisible) {
+        controls.start({ scale: 3 });
+      } else {
+        controls.start({ scale: 1 });
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScale();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [controls, className, isVisible]);
+
+  return (
+    <motion.div animate={controls} transition={{ duration: 0.3, delay: 0.5 }}>
+      {children}
+    </motion.div>
+  );
+};
 
 function App() {
   const navBarHeight = 60; // Update this with the actual height of your nav bar
@@ -122,30 +161,46 @@ function App() {
 
       <div style={contentStyle}>
         <div
-          id="home"
+          id="section1"
           style={{ ...sectionStyle, backgroundColor: "#f0f0f0" }}
         >
           <br />
-          <h1>Tom Court: Software Engineer</h1>
-        </div>
+          <div className="home-content">
+          <h1 className="home-h1">Tom Court: Software Engineer</h1>
+          <p className="home-p">Test text goes here</p>
+          </div>
+          </div>
+
         <div
-          id="about"
-          style={{ ...sectionStyle, backgroundColor: "#e0e0e0" }}
+          id="section2"
+          style={{
+            ...sectionStyle,
+            backgroundColor: "#e0e0e0",
+            position: "relative",
+          }}
         >
-          Section 2 (Height: 1000px)
+          <ScalingDiv className=".about-section">
+            <div className="about-section">
+              <h1 className="about-me-h1">ABOUT ME</h1>
+              <p className="about-me-p">Test text for when I can think of something better </p>
+              </div>
+          </ScalingDiv>
         </div>
+
         <div
-          id="projects"
+          id="section3"
           style={{ ...sectionStyle, backgroundColor: "#d0d0d0" }}
         >
           Section 3 (Height: 600px)
         </div>
+
         <div
-          id="contact"
+          id="section4"
           style={{ ...sectionStyle, backgroundColor: "#c0c0c0" }}
         >
           Section 4 (Height: 1200px)
         </div>
+
         <div
           id="section5"
           style={{ ...sectionStyle, backgroundColor: "#b0b0b0" }}
