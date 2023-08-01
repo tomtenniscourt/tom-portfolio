80056dd69f36219b2c04baa2b58c0c4270e8ccbf
Tom_Court <tomcourt94@icloud.com>
2023-08-01 10:39:40 +0100

    project title and spacing

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -55,66 +55,69 @@ const Projects = () => {
   };
 
   return (
-    <div id="projects-section">
-      {isSectionVisible && (
-        <motion.ul
-          className="container"
-          variants={container}
-          initial="hidden"
-          animate="visible"
-        >
-          {[0, 1, 2, 3].map((index) => (
-            <motion.li
-              key={index}
-              className={`item ${expandedItem === index ? "expanded" : ""}`}
-              variants={item}
-              onClick={() => handleItemClick(index)}
-              whileHover={{ scale: 1.2 }}
-            >
-              {/* Individual Content for each item */}
-              {index === 0 && (
-                <div className="content-item-one">
-                  <h2 className="item-h2">Tic Tac Toe</h2>
-                  <p className="item-p">
-                    A game of tic tac toe using simple JavaScript logic, that
-                    can be played by two users on the same computer.
-                  </p>
-                </div>
-              )}
-              {index === 1 && (
-                <div className="content-item-two">
-                  <h2 className="item-h2">Dad Jokes</h2>
-                  <p className="item-p">
-                    Using a 3rd party API, a random 'Dad Joke' is generated on
-                    the screen for the user
-                  </p>
-                </div>
-              )}
-              {index === 2 && (
-                <div className="content-item-three">
-                  <h2 className="item-h2">Weather App</h2>
-                  <p className="item-p">
-                    Another application that uses a 3rd party API - this time
-                    using real-time data to fetch weather information for any
-                    given U.S. zip code
-                  </p>
-                </div>
-              )}
-              {index === 3 && (
-                <div className="content-item-four">
-                  <h2 className="item-h2">Paint</h2>
-                  <p className="item-p">
-                    A JavaScript application that enables users to paint on a
-                    blank canvas
-                    <br />
-                  </p>
-                  
-                </div>
-              )}
-            </motion.li>
-          ))}
-        </motion.ul>
-      )}
+    <div class="project-content">
+        <div class="project-h1-div">
+        <h1 className="h1-content">PROJECTS</h1></div>
+      <div id="projects-section">
+        {isSectionVisible && (
+          <motion.ul
+            className="container"
+            variants={container}
+            initial="hidden"
+            animate="visible"
+          >
+            {[0, 1, 2, 3].map((index) => (
+              <motion.li
+                key={index}
+                className={`item ${expandedItem === index ? "expanded" : ""}`}
+                variants={item}
+                onClick={() => handleItemClick(index)}
+                whileHover={{ scale: 1.2 }}
+              >
+                {/* Individual Content for each item */}
+                {index === 0 && (
+                  <div className="content-item-one">
+                    <h2 className="item-h2">Tic Tac Toe</h2>
+                    <p className="item-p">
+                      A game of tic tac toe using simple JavaScript logic, that
+                      can be played by two users on the same computer.
+                    </p>
+                  </div>
+                )}
+                {index === 1 && (
+                  <div className="content-item-two">
+                    <h2 className="item-h2">Dad Jokes</h2>
+                    <p className="item-p">
+                      Using a 3rd party API, a random 'Dad Joke' is generated on
+                      the screen for the user
+                    </p>
+                  </div>
+                )}
+                {index === 2 && (
+                  <div className="content-item-three">
+                    <h2 className="item-h2">Weather App</h2>
+                    <p className="item-p">
+                      Another application that uses a 3rd party API - this time
+                      using real-time data to fetch weather information for any
+                      given U.S. zip code
+                    </p>
+                  </div>
+                )}
+                {index === 3 && (
+                  <div className="content-item-four">
+                    <h2 className="item-h2">Paint</h2>
+                    <p className="item-p">
+                      A JavaScript application that enables users to paint on a
+                      blank canvas
+                      <br />
+                    </p>
+                  </div>
+                )}
+              </motion.li>
+            ))}
+          </motion.ul>
+        )}
+      </div>
     </div>
   );
 };
